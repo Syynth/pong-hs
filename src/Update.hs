be710462238c88c883815d0c5c6bd6ed9d6f3989
Ben Cochrane <ben.cochrane2112@gmail.com>
2017-02-27 14:10:33 -0500

    Adds updateTime and updateKeyStates methods to Update module

--- src/Update.hs
+++ src/Update.hs
@@ -1,6 +1,20 @@
-module Update (updateGame) where
+module Update
+    ( updateGame
+    , updateTime
+    , updateKeyStates
+    ) where
 
 import Types
+
+import SFML.Window
+
+updateTime :: GameState -> IO GameState
+updateTime state = do
+        time <- getElapsedTime $ clock state
+        return (state { elapsedTime = time })
+
+updateKeyStates :: Maybe SFEvent -> GameState -> GameState
+updateKeyStates event state = state
 
 updateGame :: GameState -> GameState
 updateGame = id
